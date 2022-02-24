948934db605435d24dd9d88100df204a587f2919
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-02-24 11:01:36 -0600

    commit tree 25a4649ebc22e9d077ecf8306ca83f7eb9c142da

--- src/index.js
+++ src/index.js
@@ -12,4 +12,53 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+fetch(`http://localhost:3000/toys`).then(res => res.json()).then(data => {
+  for(let toy of data){
+    toyCard(toy)
+  }
+})
+function toyCard(toy){
+  const div = document.createElement(`div`)
+    div.classList.add(`card`)
+  const img = document.createElement(`img`)
+    img.classList.add(`toy-avatar`)
+    img.src = toy.image
+  const name = document.createElement(`h2`)
+    name.textContent = toy.name
+  const favBtn = document.createElement(`button`)
+    favBtn.classList.add(`like-btn`)
+    favBtn.id = toy.id
+    favBtn.addEventListener(`click`, e=>{
+      e.target.likes = likes++
+      e.target.textContent = `${likes} likes`
+    })
+  div.append(img, name, likes)
+  console.log(div.append(img,name,likes))
+}
+const form = document.querySelector(`.add-toy-form`)
+  form.addEventListener(`submit`, (e) =>{
+    e.preventDefault()
+    let toyName = document.getElementById(`input.name`)
+    let toyURL = document.getElementById(`input.image`)
+    let newToy = {
+      name: toyName.value,
+      image: toyURL.value,
+      likes: 0
+    }
+    toyCard(newToy)
+  })
+
+const likes = (e) =>{
+  const likeCount = parseInt(e.target.previousSibling.innerText) + 1
+  fetch(`http://localhost:3000/toys/${e.target.id}`,{
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body:JSON.stringify({
+      "likes" : likeCount
+    })
+  })
+}
+})
